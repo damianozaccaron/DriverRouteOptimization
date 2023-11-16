cd1173e1d60b3165dca22d7600c8888009b78a9e
enricoguerriero <enrico.guerriero@gmail.com>
2023-11-16 17:24:43 +0100

    prova

--- src/generator/route_generator.py
+++ src/generator/route_generator.py
@@ -1,14 +1,17 @@
 import random
 import string
 
-def generate_standard_routes(sr_count, provinces_count): 
+# Ho aggiunto 2 nuovi argomenti: 
+# n_merc numero di oggetti di merchandise da mettere in ogni camion
+# tot_merc dimensione del pool degli oggetti
+def generate_standard_routes(sr_count, provinces_count, n_merch, tot_merch): 
     with open("src/generator/data/province.csv", "r") as csv_file:
         provinces = csv_file.readlines()
     provinces = provinces[0].split(",")
     random_provinces = random.choices(provinces, k = provinces_count)
 
     merchandise = []
-    for i in range(10):
+    for i in range(tot_merch):
         merch = "".join(random.choices(string.ascii_lowercase,
                                         k = random.randint(3, 9)))
         merchandise.append(merch)
@@ -25,13 +28,25 @@ def generate_standard_routes(sr_count, provinces_count):
         previous_end_index = end_province_index
 
         selected_merch = []
-        for j in range(5):
-            merch = merchandise[random.randint(0, 9)]
+        # inserisco un po' di randomicità nel numero di oggetti
+        cacca = random.randint(0,10)
+        if cacca == 0:
+            pupu = n_merch - 2
+        elif cacca < 3:
+            pupu = n_merch - 1
+        elif cacca < 8:
+            pupu = n_merch
+        elif cacca < 10:
+            pupu = n_merch + 1
+        else:
+            cacca = n_merch + 2
+        for j in range(pupu):
+            merch = merchandise[random.randint(0, tot_merch)]
             if merch not in selected_merch:
                 selected_merch.append(merch)
         selected_merch_values = {}
         for m in selected_merch:
-            selected_merch_values[m] = random.randint(0, 30)
+            selected_merch_values[m] = random.randint(1, 30)
             
         standard_route["id"] = "sr_" + str(i)
         standard_route["from"] = start_province
