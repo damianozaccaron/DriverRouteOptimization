843cc2854897d743a0d48e60b2f2dee57027cf69
andreleo02 <andreleo02@gmail.com>
2023-12-27 16:50:42 +0100

    pyspark integration

--- src/main.py
+++ src/main.py
@@ -1,10 +1,46 @@
 # imports
+import os
 from dotenv import load_dotenv
 import json
 
+import findspark
 
+from generator.route_generator import actual_routes_generator, json_writer, merchandise_generator, standard_routes_generator
+
+findspark.init()
+
+from pyspark.ml.clustering import KMeans
+from pyspark.sql import SparkSession
 
 load_dotenv()
+
+provinces_count = int(os.environ.get("PROVINCES_TO_PICK", 10))
+n_merchandise = int(os.environ.get("NUMBER_OF_ITEMS_PER_TRIP", 3))
+tot_merchandise = int(os.environ.get("TOTAL_NUMBER_OF_ITEMS", 10))
+sr_count = int(os.environ.get("STANDARD_ROUTES_COUNT", 1))
+
+merchandise = merchandise_generator(20)
+print("merchandise generated")
+
+standard_routes = standard_routes_generator(sr_count, provinces_count, n_merchandise, merchandise, 5)
+print("sr generated")
+
+actual_routes = actual_routes_generator(standard_routes, merchandise, 20, 15)
+json_writer(actual_routes, "src/generator/data/actual_routes.json")
+print("ac generated")
+
+spark = SparkSession.builder \
+        .master("local") \
+        .appName(name = "Python Spark SQL basic example") \
+        .getOrCreate()
+
+dataframe = spark.read.json(path = "src/generator/data/actual_routes.json")
+data = spark.createDataFrame(data = actual_routes)
+kmeans = KMeans().setK(sr_count).setSeed(1).setFeaturesCol("route")
+model = kmeans.fit(data)
+print("model fitted")
+
+
 
 # os.environ.get
 # load the value of the variable in the first argument defined in the .env file
@@ -269,4 +305,7 @@ print(actual_routes_as_points[0].coordinates)
 
 # 2. generate output 2
 # 3. generate output 3
+# 3. generate output 1
+# 4. generate output 2
+# 5. generate output 3
 
