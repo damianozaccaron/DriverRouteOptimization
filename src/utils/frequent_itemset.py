5f1b54a48a595c308ec1475373d61d2138630419
Damiano Zaccaron <74066074+damianozaccaron@users.noreply.github.com>
2024-01-08 11:29:07 +0100

    Aggiunto codice frequent itemset

---
+++ src/utils/frequent_itemset.py
@@ -0,0 +1,114 @@
+import time
+import itertools
+
+import merch
+import trips
+
+
+def hash_pair(n1, n2, n_buckets):
+    """Generates a basic hash function starting from string"""
+    if isinstance(n1, tuple):
+        n1 = n1[0] + n1[1]
+    if isinstance(n2, tuple):
+        n2 = n2[0] + n2[1]
+    ascii1 = [ord(char) for char in n1]
+    ascii2 = [ord(char) for char in n2]
+    "hash function is the module of the sum of the total ASCII values of the parameters divided by number of buckets"
+    return ((sum(ascii1)**2) + (sum(ascii2)**2)) % n_buckets
+
+
+def pcy_basket(basket, n_buckets, pairs_hashtable, singletons):
+    """Does the first pass of the PCY for a single basket, its only use is to modify its parent's dictionaries"""
+
+    "count frequency of the items"
+    for item in basket:
+        singletons[item] = singletons.get(item, 0) + 1
+
+    "creates the couples with itertools (tuple) and adds them to a dictionary with the respective count"
+    for key in itertools.combinations(singletons, 2):
+        "Applying hash function"
+        hash_value = hash_pair(key[0], key[1], n_buckets)
+        "If the hash value is present we add to its count"
+        pairs_hashtable[hash_value] = pairs_hashtable.get(hash_value, 0) + 1
+
+
+def run_pcy(baskets, n_buckets, t_hold, start=time.time()):
+    print('yeah')
+    singletons = {}
+    pairs_count_hash = {}
+
+    for item in baskets:
+        pcy_basket(item, n_buckets, pairs_count_hash, singletons)
+    "remove singletons that are not frequent"
+    frequent_single_items = {}
+    for key in singletons.items():
+        if key[1] >= len(baskets) * t_hold:
+            "discuss about threshold"
+            frequent_single_items[key[0]] = key[1]
+
+    "creates a list containing only the names of the frequent items (remove the count)"
+    frequent_singletons = [item for item in frequent_single_items]
+
+    # BETWEEN PASSES
+    bitmap = [0] * n_buckets  # bitmap that will summarise which baskets have the minimum support
+    "transform into a bit vector with value 1 if the count is over the threshold"
+    for key in sorted(pairs_count_hash.keys()):
+        if pairs_count_hash[key] > len(baskets) * t_hold:
+            bitmap[key] = 1
+
+    # PASS 2
+    "we only keep the pairs that have frequent singletons and belong to a frequent bucket"
+    candidate_pairs = {}
+    for i in range(0, len(frequent_singletons)):
+        for j in range(i+1, len(frequent_singletons)):
+            "we find out if the pair belongs to a frequent bucket"
+            hash_value = hash_pair(frequent_singletons[i], frequent_singletons[j], n_buckets)
+            if bitmap[hash_value] > 0:
+                "if it belongs to a frequent bucket we consider it as a candidate"
+                candidate_pair = (frequent_singletons[i], frequent_singletons[j])
+                candidate_pairs[candidate_pair] = candidate_pairs.get(candidate_pair, 0) + 1
+
+    "now we have all the candidate pairs, we want to count how much they appear together in the same route"
+    frequent_pairs = {}
+    for key in candidate_pairs:
+        for item in baskets:
+            if key[0] in item and key[1] in item:
+                frequent_pairs[(key[0], key[1])] = frequent_pairs.get((key[0], key[1]), 0) + 1
+
+    "We cancel entries that are not actually frequent"
+    temp = [item[0] for item in frequent_pairs.items() if item[1] < len(baskets) * support_threshold]
+    for item in temp:
+        del frequent_pairs[item]
+
+    "Timestamp"
+    print('Generated frequent pairs in: ', time.time() - start, 'seconds')
+
+    return frequent_pairs
+
+
+"prove"
+"import data for a specific driver"
+data = trips.import_data('actual.json', 'N71YE')
+
+num_buckets = 30  # check
+support_threshold = 0.2
+
+# frequent itemset cities
+x = run_pcy(trips.extract_destinations(data), n_buckets=num_buckets, t_hold=support_threshold, start=time.time())
+print(x)
+print('len: ', len(x))
+
+# frequent itemset trips, probably needs lower threshold than cities
+y = run_pcy(trips.extract_trips_path(data), n_buckets=num_buckets, t_hold=support_threshold, start=time.time())
+print(y)
+print('len: ', len(y))
+
+# frequent itemset merchandise, lower threshold
+z = run_pcy(merch.extract_merchandise_type(data), n_buckets=num_buckets, t_hold=support_threshold, start=time.time())
+print(z)
+print('len: ', len(z))
+
+# frequent itemset associating city - trip
+luciano = run_pcy(merch.extract_merch_city(data), n_buckets=num_buckets, t_hold=0.1, start=time.time())
+print(luciano)
+print('len: ', len(luciano))
