d4d0793476639b48e108864c1f245411b730e08b
DamianoZaccaron <damianozaccaron@gmail.com>
2024-01-12 15:42:46 +0100

    cocacola

--- src/damiANO.py
+++ src/damiANO.py
@@ -2,55 +2,11 @@ import json
 import time
 import utils.frequent_itemset
 import statistics
+from utils.functions import get_actual_route  # list of actual routes
+from entities.actual_route import ActualRoute
+import collections
 
-
-
-class Preferences:
-    """contains the preferences of each driver"""
-
-    def __init__(self, freq_city: list, freq_start: list, freq_finish: list, freq_trip: list, freq_itemset_city: list, freq_itemset_trip: list, n_trip: float, 
-                 freq_merch_avg: float, n_merch: list, n_merch_per_route: float, freq_merch_per_trip: list):
-        self.freq_city = freq_city  # dict(key = string, value = int), lista di città per cui è passato spesso
-        self.freq_start = freq_start  # dict(key = string, value = int), lista di città da cui è partito spesso
-        self.freq_finish = freq_finish  # dict(key = string, value = int), lista di città in cui è arrivato spesso
-        self.freq_trip = freq_trip  # dict(key = tuple, value = int), lista di trip effettuati spesso
-        self.freq_itemset_city = freq_itemset_city  # dict(key = tuple, value = int), freq itemset di città
-        self.freq_itemset_trip = freq_itemset_trip  # dict(key = tuple(tuple), value = int), freq itemset di trip
-        self.n_trip = n_trip  # int, trip medi per route
-        self.freq_merch_avg = freq_merch_avg  # int, numero medio di classi di merce per trip
-        self.n_merch = n_merch  # dict(key = string, value = int), lista di merci che ha portato spesso
-        self.n_merch_per_route = n_merch_per_route  # int, numero medio di merce (quantità totale) portata per trip
-        self.freq_itemset_per_trip = freq_merch_per_trip   # dict(key = tuple(tuple), value = int), freq itemset di
-        # merci per trip
-        "se serve lo faccio per città: "
-        # self.freq_itemset_per_city = freq_merch_per_trip  # dict(key = tuple(tuple), value = int)
-
-
-def implement_pref(data, output_len):
-    pref = Preferences(
-        freq_city=sorted(pass_through_city_count(data).items(), key=lambda item: item[1], reverse=True)[
-                  0:output_len],
-        freq_start=sorted(start_finish_count(data, 0).items(), key=lambda item: item[1], reverse=True)[
-                   0:output_len],
-        freq_finish=sorted(start_finish_count(data).items(), key=lambda item: item[1], reverse=True)[
-                    0:output_len],
-        freq_trip=sorted(trip_count(data).items(), key=lambda item: item[1], reverse=True)[0:output_len],
-        freq_itemset_city=sorted(utils.frequent_itemset.run_pcy(extract_destinations(data), n_buckets=600, t_hold=0.3,
-                                                          start=time.time()).items(), key=lambda item: item[1],
-                                 reverse=True)[0:output_len],
-        freq_itemset_trip=sorted(utils.frequent_itemset.run_pcy(extract_trips_path(data), n_buckets=600, t_hold=0.2,
-                                                          start=time.time()).items(), key=lambda item: item[1],
-                                 reverse=True)[0:output_len],
-        n_trip=mean_trip(data),
-        freq_merch_avg=mean_types(data),
-        n_merch=sorted(count_merch(data).items(), key=lambda item: item[1], reverse=True)[0:output_len],
-        n_merch_per_route=mean_quantities(data),
-        freq_merch_per_trip=sorted(
-            utils.frequent_itemset.run_pcy(extract_merchandise_type(data), n_buckets=600, t_hold=0.2,
-                                     start=time.time()).items(), key=lambda item: item[1], reverse=True)[0:output_len]
-    )
-
-    return pref
+data = get_actual_route()
 
 
 def import_data(file, driver):
@@ -61,15 +17,18 @@ def import_data(file, driver):
 
 
 def count_occurrences(obj, spec, spec2=None):
-    """counts how many times a specific object appears in a route and puts the value in a dictionary"""
+    """counts how many times a specific object appears in a route and puts the value in a dictionary.
+
+    Requires a trip (obj) and a specification (e.g. city_from) as input. If both city_from and city_to are passed,
+    it counts how many times that trip has been performed"""
     occ = {}
     if spec2 is None:
         for item in obj:
-            element = item[spec]
+            element = item.spec
             occ[element] = occ.get(element, 0) + 1
     else:
         for item in obj:
-            element = [item[spec], item[spec2]]
+            element = [item.spec, item.spec2]
             element = tuple(element)
             occ[element] = occ.get(element, 0) + 1
 
@@ -81,13 +40,13 @@ def extract_route(aroute):
     return aroute.route
 
 
-def extract_trips(var):
-    """extracts the single trips from a set of routes, it also adds the position of the trip inside its route"""
+def extract_trips(var: list[ActualRoute]):
+    """extracts the single trips from a set of routes"""
     trips = []
-    for elem in var:
-        for i, item in enumerate(extract_route(elem)):
-            trip_data = {'trip': item, 'pos': i}
-            trips.append(trip_data)
+    for act_route in var:  # actual route
+        for trip in act_route.route:
+            trips.append(trip)
+
     return trips
 
 
@@ -103,26 +62,21 @@ def pass_through_city_count(var):
     return count_occurrences(result, 'from')
 
 
-def start_finish_count(var, s_or_f=1):
+def start_finish_count(var: list[ActualRoute], s_or_f=1):
     """computes the number of times a driver starts or finish from a given city
 
     s_or_f determines if it returns the cities where he started or the cities where he finished, s or 0 indicates
-    start otherwise it's finish (default)"""
+    start otherwise it's finish (default). Requires a list of actual routes as input."""
     result = []
-    for i, item in enumerate(extract_trips(var)):
+    for act_route in var:
         "if looking for start"
         if s_or_f == 0 or s_or_f == 's':
-            if item['pos'] == 0:
-                result.append(item)
+            result.append(act_route.route[0].city_from)
             "if looking for finish"
-        elif i < len(extract_trips(var)) - 1:
-            if extract_trips(var)[i]['pos'] > extract_trips(var)[i+1]['pos']:
-                result.append(item)
+        else:
+            result.append(act_route.route[-1].city_to)
 
-    if s_or_f == 0 or s_or_f == 's':
-        return count_occurrences(result, 'from')
-    else:
-        return count_occurrences(result, 'to')
+    return collections.Counter(result)
 
 
 def trip_count(var):
