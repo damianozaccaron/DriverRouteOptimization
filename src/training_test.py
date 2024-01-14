0f7e614606d73e9438f6485a21d583ce9b98fe93
enricoguerriero <enrico.guerriero@gmail.com>
2024-01-14 15:16:05 +0100

    aaa

--- src/training_test.py
+++ src/training_test.py
@@ -2,7 +2,7 @@
 # let's try it
 
 # imports for functions 
-from utils.functions import get_actual_routes, save_run_parameters, get_standard_routes
+from utils.functions import get_actual_routes, save_run_parameters, get_standard_routes, get_coordinates_path
 from utils.route_generator import data_generation
 from preferoute import preferoute_similarity
 from entities.coordinate_system import CoordinateSystem
@@ -12,6 +12,7 @@ from entities.actual_route import ActualRoute
 # imports libraries
 import time
 import random
+import os
 
 
 # functions:
@@ -36,13 +37,13 @@ def recommended_standard_route_generator_check(actual_routes: list[ActualRoute],
     spark = SparkSession.builder.master("local").appName(name="PySpark for clustering").getOrCreate()
 
     # import functions
-    from first_point import create_space, write_coordinates, build_centers, normalize_cluster_centers, \
-        build_result, parameters_extraction, create_clusters
+    from first_point import create_space, build_centers, normalize_cluster_centers, \
+        build_result, parameters_extraction, create_clusters, read_coordinates
 
     parameters = parameters_extraction(standard_routes=standard_routes)
 
     # creation of the space with train actual routes
-    space = create_space(actual_routes=actual_routes_train)
+    space = create_space(actual_routes=actual_routes)
 
     # writing coordinates of every train actual routes (according to the space) on a .csv file
     write_coordinates(actual_routes=actual_routes_train, space=space)
@@ -62,41 +63,185 @@ def recommended_standard_route_generator_check(actual_routes: list[ActualRoute],
     recommended_standard_route = build_result(normalized_centers=norm_centers, actual_routes=actual_routes_train,
                                               model=model, spark=spark)
 
-    between_and_within_var(model, spark)
+    # writing data of original standard route
+    write_coordinates(actual_routes = standard_routes, space = space)
 
-    # creating test space
-    test_space = create_space(actual_routes=actual_routes_train)
+    # reading data for standard route
+    sr_data = read_coordinates(spark)
 
     # writing coordinates of test acutal routes
-    write_coordinates(actual_routes=actual_routes_test, space=test_space)
+    write_coordinates(actual_routes=actual_routes_test, space=space)
 
     # compute the distance between test set and rec_sr
+    dist = distance_from_centers(cluster_centers = centers, spark = spark)
 
+    # compute the distance between test set and sr
+    dist_origin = distance_from_sr(standard_routes = sr_data, spark = spark)
 
+    print(dist/dist_origin)
 
     spark.stop()
 
-
-
     return recommended_standard_route
 
-
-def between_and_within_var(model, spark):
+import math
+def distance_from_centers(cluster_centers, spark):
     from first_point import read_coordinates
-    from pyspark.sql.functions import col, expr
+    from pyspark.sql.functions import col
+    from pyspark.sql.types import StructType, StructField, FloatType
+    from pyspark.ml.feature import VectorAssembler
+    from pyspark.ml.linalg import DenseVector
+    import numpy as np
 
     data = read_coordinates(spark)
+    
+    field_names = cluster_centers[0].keys()
+    schema = StructType([StructField(field, FloatType(), True) for field in field_names])
+    cluster_centers = spark.createDataFrame([{field: float(value) if isinstance(value, (int, float)) else value for field, value in d.items()} for d in cluster_centers], schema=schema)
+    
+    feature_cols = cluster_centers.columns[1:]
+    vec_assembler = VectorAssembler(inputCols=feature_cols, outputCol="features")
+    df = vec_assembler.transform(cluster_centers)   
+    df = df.select("pred", "features")
 
-    predictions = model.transform(data)
-    centroids = model.clusterCenters()
-    within_variance = predictions.withColumn("squared_distance", expr("POWER(prediction - features[0], 2)")).groupBy().sum("squared_distance").collect()[0][0]
-    print(f"Within-cluster variance: {within_variance}")
+    feature_cols = data.columns[1:]
+    vect_assembler = VectorAssembler(inputCols=feature_cols, outputCol="data_features")
+    data = vect_assembler.transform(data)   
+    data = data.select("id", "data_features")
 
-    mean_exprs = [expr(f"AVG(features[{i}])").alias(f"avg_features_{i}") for i in range(len(centroids[0]))]
-    mean_features = data.select(*mean_exprs).collect()[0]
-    overall_mean = [mean_features[f"avg_features_{i}"] for i in range(len(centroids[0]))]
-    between_variance = sum([sum([(c - m) ** 2 for c, m in zip(centroid, overall_mean)]) for centroid in centroids])
-    print(f"Between-cluster variance: {between_variance}")
+    dist = []
+    for point in data.select("data_features").collect():
+        all_dist = []
+        for center in df.select("features").collect():
+            dense_center = DenseVector(center)
+            dense_point = DenseVector(point)
+            a_center = np.array(dense_center)[0]
+            a_point = np.array(dense_point)[0][:math.floor(len(data.select("data_features").collect()[0][0])/2)]
+            single_dist = euclidean_distance_udf(a_center, a_point)
+            all_dist.append(single_dist)
+        min_distance = min(all_dist)
+        dist.append(min_distance)
+    
+    print(dist)
+
+
+def distance_from_sr(standard_routes, spark):
+    from first_point import read_coordinates
+    from pyspark.sql.functions import col
+    from pyspark.sql.types import StructType, StructField, FloatType
+    from pyspark.ml.feature import VectorAssembler
+    from pyspark.ml.linalg import DenseVector
+    import numpy as np
+
+    data = read_coordinates(spark)
+    df = standard_routes
+    feature_cols = data.columns[1:]
+    vect_assembler = VectorAssembler(inputCols=feature_cols, outputCol="data_features")
+    data = vect_assembler.transform(data)   
+    data = data.select("id", "data_features")
+
+    dist = []
+    for point in data.select("data_features").collect():
+        all_dist = []
+        for center in df.select("features").collect():
+            dense_center = DenseVector(center)
+            dense_point = DenseVector(point)
+            a_center = np.array(dense_center)[0]
+            a_point = np.array(dense_point)[0][:math.floor(len(data.select("data_features").collect()[0][0])/2)]
+            single_dist = euclidean_distance_udf(a_center, a_point)
+            all_dist.append(single_dist)
+        min_distance = min(all_dist)
+        dist.append(min_distance)
+    
+    print(dist)
+
+
+
+
+def euclidean_distance_udf(point, fixed_point):
+    import numpy as np
+    return np.sqrt(np.sum((point - fixed_point) ** 2))
+
+
+import csv
+
+def write_coordinates(actual_routes: list[ActualRoute], space: CoordinateSystem):
+    header: list[str] = []
+    header.append("id")
+    header.extend(space.all_city_vec)
+    header.extend(space.all_merch)
+    header.extend(space.all_trip)
+
+    # Check if the file already exists
+    file_exists = os.path.exists(get_coordinates_path())
+
+    with open(get_coordinates_path(), "a" if file_exists else "w") as f:
+        writer = csv.writer(f)
+
+        # Write the header only if the file is newly created
+        if not file_exists:
+            writer.writerow(header)
+
+        for ar in actual_routes:
+            row_result = []
+            row_result.append(ar.id)
+            actual_route_cities = ar.extract_city()
+            for city in space.all_city_vec:
+                row_result.append(5 if city in actual_route_cities else 0)
+            actual_route_merch = ar.extract_merch()
+            total_quant = sum(actual_route_merch.quantity)
+            for merch in space.all_merch:
+                if merch in actual_route_merch.item:
+                    index = actual_route_merch.item.index(merch)
+                    row_result.append(actual_route_merch.quantity[index] / total_quant)
+                else:
+                    row_result.append(0)
+            actual_route_trips = ar.trip_string()
+            for trip in space.all_trip:
+                row_result.append(10 if trip in actual_route_trips else 0)
+            writer.writerow(row_result)
+
+
+# NON FUNZIONA DIO CANE
+# def between_and_within_var(model, spark):
+#     from first_point import read_coordinates
+#     from pyspark.sql.functions import col, expr, var_samp
+
+#     data = read_coordinates(spark)
+
+#     # predictions = model.transform(data)
+#     # centroids = model.clusterCenters()
+#     # within_variance = predictions.withColumn("squared_distance", expr("POWER(prediction - features[0], 2)")).groupBy().sum("squared_distance").collect()[0][0]
+#     # print(f"Within-cluster variance: {within_variance}")
+
+#     # mean_exprs = [expr(f"AVG(features[{i}])").alias(f"avg_features_{i}") for i in range(len(centroids[0]))]
+#     # mean_features = data.select(*mean_exprs).collect()[0]
+#     # overall_mean = [mean_features[f"avg_features_{i}"] for i in range(len(centroids[0]))]
+#     # between_variance = sum([sum([(c - m) ** 2 for c, m in zip(centroid, overall_mean)]) for centroid in centroids])
+#     # print(f"Between-cluster variance: {between_variance}")
+
+#     # Extract cluster labels as a PySpark DataFrame
+#     cluster_labels_df = model.transform(data).select("id", "prediction")
+
+#     cluster_labels_df = cluster_labels_df.withColumnRenamed("id", "cluster_id")
+
+#     # Join the cluster labels with the original data
+#     data_with_labels = data.join(cluster_labels_df, col("id") == col("id"))
+
+#     print(data_with_labels.columns)
+
+#     # Calculate within-cluster variance
+#     within_variance = (
+#         data_with_labels
+#         .groupBy("prediction")
+#         .agg(
+#             var_samp(features)
+#         )
+#         # .agg(sum("within_cluster_variance").alias("total_within_variance"))
+#         # .collect()[0]["total_within_variance"]
+#     )
+    
+#     print("Within-cluster Variance (Sum of Squared Distances):", within_variance)
 
 
 
